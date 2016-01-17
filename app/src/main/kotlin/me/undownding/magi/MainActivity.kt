c20c35df75e1a6cec27a8f6a121c29c63b08cb66
undownding <undownding@gmail.com>
2016-01-17 19:55:05 +0800

    MainActivity UI

--- app/src/main/kotlin/me/undownding/magi/MainActivity.kt
+++ app/src/main/kotlin/me/undownding/magi/MainActivity.kt
@@ -2,11 +2,20 @@ package me.undownding.magi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
 
+    private val MAGI_INDEX_URL = "http://magi.peak-labs.com/"
+
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl(MAGI_INDEX_URL)
     }
 }
